4a6a0e389f9838807cca72c61e26b2bd37ce2edd
PriyanshuSingh <priyanshusingh05101994@gmail.com>
2016-06-18 11:59:52 +0530

    keyboard control

--- Classes/Player.cpp
+++ Classes/Player.cpp
@@ -9,34 +9,64 @@ bool Player::init() {
     if(!Node::init()){
         return false;
     }
+    {
+        auto listener = EventListenerTouchOneByOne::create();
+        listener->onTouchBegan = [this](Touch *touch, Event *unused_event) -> bool {
 
-    auto listener = EventListenerTouchOneByOne::create();
-    listener->onTouchBegan = [this](Touch *touch, Event *unused_event)->bool{
+            auto location = touch->getLocation();
 
-        auto location = touch->getLocation();
+            if (location.x <= _director->getWinSize().width / 2) {
+                orientation = Movement::Left;
+            }
+            else {
+                orientation = Movement::Right;
 
-        if(location.x<=_director->getWinSize().width/2){
-            orientation = Movement::Left;
-        }
-        else{
-            orientation = Movement::Right;
+            }
+            return true;
 
-        }
-        return true;
+        };
+        listener->onTouchMoved = [this](Touch *touch, Event *unused_event) {
 
-    };
-    listener->onTouchMoved = [this](Touch *touch, Event *unused_event){
+        };
+        listener->onTouchEnded = [this](Touch *touch, Event *unused_event) {
 
-    };
-    listener->onTouchEnded = [this](Touch *touch, Event *unused_event){
+            orientation = Movement::Normal;
+        };
+        listener->onTouchCancelled = [this](Touch *touch, Event *unused_event) {
+            orientation = Movement::Normal;
+        };
 
-        orientation = Movement::Normal;
-    };
-    listener->onTouchCancelled = [this](Touch *touch, Event *unused_event){
-        orientation = Movement::Normal;
-    };
+        _eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+    }
 
-    _eventDispatcher->addEventListenerWithSceneGraphPriority(listener,this);
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_MAC || CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
+    {
+        auto listener = EventListenerKeyboard::create();
+        listener->onKeyPressed = [this](EventKeyboard::KeyCode keyCode, Event *event) -> bool{
+
+            if (keyCode == EventKeyboard::KeyCode::KEY_LEFT_ARROW) {
+                orientation = Movement::Left;
+            }
+            else if(keyCode == EventKeyboard::KeyCode::KEY_RIGHT_ARROW){
+                orientation = Movement::Right;
+            }
+            return true;
+        };
+
+        listener->onKeyReleased = [this](EventKeyboard::KeyCode keyCode, Event *event) -> bool{
+            if (keyCode == EventKeyboard::KeyCode::KEY_LEFT_ARROW) {
+                orientation = Movement::Normal;
+            }
+            else if(keyCode == EventKeyboard::KeyCode::KEY_RIGHT_ARROW){
+                orientation = Movement::Normal;
+            }
+            return true;
+        };
+
+        _eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+    }
+#endif
+
 
     displaySprite = Sprite::create("Triangle.png");
     addChild(displaySprite);
